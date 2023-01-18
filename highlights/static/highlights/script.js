ef23d51fa4f6d9bf3d41cd89b74edb8a46468d9d
shrestrbl <shrestrbl@gmail.com>
2023-01-17 21:15:20 -0500

    added toggle option to notice thread using js

--- highlights/static/highlights/script.js
+++ highlights/static/highlights/script.js
@@ -1,3 +1,4 @@
+//highlights current playing video from the playlist
 document.addEventListener('DOMContentLoaded', function() {
     let el2 = document.querySelector('.iframe')
     document.querySelectorAll('img').forEach(function(img) {
@@ -5,4 +6,20 @@ document.addEventListener('DOMContentLoaded', function() {
         if (link == el2.dataset.vidlink)
             img.style.border = 'solid orange 2px'
     });
+});
+
+//toggle notice thread
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.fa-solid').onclick = function() {
+        const x = document.querySelector('.indexnotice')
+        const y = document.getElementById("toggle")
+        if (x.style.display ==="none") {
+            x.style.display = "block";
+            y.className = "fa-solid fa-angle-up";
+            }
+        else {
+            x.style.display = "none";
+            y.className = "fa-solid fa-angle-down";
+            }
+    }
 });
