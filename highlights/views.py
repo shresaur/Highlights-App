8786c9ca2e196b66526aff2df3ed2665bc8f1a8a
shrestrbl <shrestrbl@gmail.com>
2023-05-04 21:58:31 -0400

    Created YoutubePlayList class in views

--- highlights/views.py
+++ highlights/views.py
@@ -12,41 +12,50 @@ VIDEO_ENDPOINT = "https://www.googleapis.com/youtube/v3/videos"
 PL_PLAYLIST_ID = "PLXEMPXZ3PY1hjUnuEJqxxYoRQLOEw6WFj"
 
 
-def data_playlist(playlist_id, title=None):
-    playlist_params = {"key": KEY, "part": "snippet", "playlistId": playlist_id, "maxResults": "64"}
-    response = requests.get(PLAYLIST_ENDPOINT, params=playlist_params)
-    data = response.json()
-    data_list = []
-    next_page = True
-    while next_page:
-        for i in data["items"]:
-            if title:
-                if title in i["snippet"]["title"]:
-                    data_dict = {"thumbnail": i["snippet"]["thumbnails"]["high"]["url"],
-                                 "videoid": i["snippet"]["resourceId"]["videoId"]}
-                    data_list.append(data_dict)
-                else:
-                    pass
-            else:
+class YouTubePlaylist:
+    def __init__(self, playlist_id, api_key):
+        self.playlist_id = playlist_id
+        self.api_key = api_key
+        self.playlist_endpoint = 'https://www.googleapis.com/youtube/v3/playlistItems'
+
+    def get_video_data(self, title=None):
+        """
+        Retrieve video data for a YouTube playlist.
+
+        Parameters:
+        title (str, optional): If specified, only videos with titles containing this string will be included.
+
+        Returns:
+        A list of dictionaries, where each dictionary contains information about a single video in the playlist.
+        Each dictionary contains the following keys:
+        - thumbnail (str): The URL of the video's thumbnail image.
+        - videoid (str): The ID of the YouTube video.
+        """
+        playlist_params = {"key": self.api_key, "part": "snippet", "playlistId": self.playlist_id, "maxResults": "64"}
+        response = requests.get(self.playlist_endpoint, params=playlist_params)
+        data = response.json()
+        video_data = self._parse_data(data, title)
+        while "nextPageToken" in data:
+            token = data["nextPageToken"]
+            playlist_params["pageToken"] = token
+            response = requests.get(self.playlist_endpoint, params=playlist_params)
+            data = response.json()
+            video_data += self._parse_data(data, title)
+        return video_data
+
+    def _parse_data(self, data, title=None):
+        data_list = []
+        for i in data.get("items", []):
+            if not title or title in i["snippet"]["title"]:
                 data_dict = {"thumbnail": i["snippet"]["thumbnails"]["high"]["url"],
                              "videoid": i["snippet"]["resourceId"]["videoId"]}
                 data_list.append(data_dict)
-        try:
-            if data["nextPageToken"]:
-                token = data["nextPageToken"]
-                playlist_params = {"key": KEY, "part": "snippet", "playlistId": playlist_id, "maxResults": "64",
-                                   "pageToken": token}
-                response = requests.get(PLAYLIST_ENDPOINT, params=playlist_params)
-                data = response.json()
-            else:
-                pass
-        except KeyError:
-            next_page = False
-    return data_list
+        return data_list
 
+# Get premier league playlist data
+playlist = YouTubePlaylist(playlist_id=PL_PLAYLIST_ID, api_key=KEY)
+video_data_pl = playlist.get_video_data(title="| PREMIER LEAGUE HIGHLIGHTS |")
 
-# To get Premier League data
-data_list_pl = data_playlist(PL_PLAYLIST_ID, "| PREMIER LEAGUE HIGHLIGHTS |")
 
 
 def video_list():
@@ -82,9 +91,9 @@ def premier_league(request, videolink):
     main_desc = "Gabriel Martinelli netted the first goal of the 2022-23 Premier League campaign to lift Arsenal" \
                 " to a season-opening victory against Crystal Palace."
     date = "Aug 5, 2022"
-    infolist = data_list_pl
+    info_list = video_data_pl
     return render(request, "highlights/playepisode.html",
-                  {"link": videolink, "data": infolist, "maintitle": main_title, "maindescription": main_desc,
+                  {"link": videolink, "data": info_list, "maintitle": main_title, "maindescription": main_desc,
                    "publishdate": date, "page": "PL"})
 
 
